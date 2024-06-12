fb8291cedc5b81bf5caaa23846e5bcce69b15129
kuzum09 <kp922004@gmail.com>
2024-06-12 16:44:37 +0530

    minorchnages

--- src/components/Home/Type.js
+++ src/components/Home/Type.js
@@ -2,21 +2,20 @@ import React from "react";
 import Typewriter from "typewriter-effect";
 
 function Type() {
-  return (
-    <Typewriter
-      options={{
-        strings: [
-          "Software Developer",
-          "Freelancer",
-          "MERN Stack Developer",
-          "Open Source Contributor",
-        ],
-        autoStart: true,
-        loop: true,
-        deleteSpeed: 50,
-      }}
-    />
-  );
+	return (
+		<Typewriter
+			options={{
+				strings: [
+					"B-tech Student at IIIT Naya Raipur...",
+					"My Branch is Data Science and AI :)",
+					"I am a passionate Web Devloper !!",
+				],
+				autoStart: true,
+				loop: true,
+				deleteSpeed: 80,
+			}}
+		/>
+	);
 }
 
 export default Type;
